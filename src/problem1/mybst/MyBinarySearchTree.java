5f2bddab3a56bb903d1e6850091270362d5de808
pallavisolanki2000 <pallavi.solanki_cs18@gla.ac.in>
2020-03-28 12:20:36 +0530

    created  getter and setter method

--- src/problem1/mybst/MyBinarySearchTree.java
+++ src/problem1/mybst/MyBinarySearchTree.java
@@ -32,4 +32,53 @@ public class MyBinarySearchTree {
             newnode = null;
         }
     }
+
+    public TreeNode getRoot() {
+        return root;
+    }
+
+    public void setRoot(TreeNode root) {
+        this.root = root;
+    }
+
+    public TreeNode getNewnode() {
+        return newnode;
+    }
+
+    public void setNewnode(TreeNode newnode) {
+        this.newnode = newnode;
+    }
+
+    public TreeNode getTmp() {
+        return tmp;
+    }
+
+    public void setTmp(TreeNode tmp) {
+        this.tmp = tmp;
+    }
+
+    public MyQueue getPre() {
+        return pre;
+    }
+
+    public void setPre(MyQueue pre) {
+        this.pre = pre;
+    }
+
+    public MyQueue getPost() {
+        return post;
+    }
+
+    public void setPost(MyQueue post) {
+        this.post = post;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int ctr) {
+        MyBinarySearchTree.count = ctr;
+    }
+
 }
