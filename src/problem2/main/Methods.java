1adcfb35b0370a8cfe94458c18f1e9cb120f653e
pallavisolanki2000 <pallavi.solanki_cs18@gla.ac.in>
2020-03-28 12:43:57 +0530

    created constructon and getter nd setter

--- src/problem2/main/Methods.java
+++ src/problem2/main/Methods.java
@@ -7,4 +7,35 @@ public class Methods {
     private MyBinarySearchTree m;
 
     private MyQueue pre, post;
+
+    public Methods() {
+        m = new MyBinarySearchTree();
+        pre = new MyQueue();
+        post = new MyQueue();
+    }
+
+    public MyQueue getPre() {
+        return pre;
+    }
+
+    public void setPre(MyQueue pre) {
+        this.pre = pre;
+    }
+
+    public MyQueue getPost() {
+        return post;
+    }
+
+    public void setPost(MyQueue post) {
+        this.post = post;
+    }
+
+    public MyBinarySearchTree getM() {
+        return m;
+    }
+
+    public void setM(MyBinarySearchTree m) {
+        this.m = m;
+    }
+
 }
