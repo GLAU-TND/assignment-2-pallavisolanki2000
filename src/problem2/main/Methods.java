004d7da3904655aa65724515d697fdf4613b7b10
pallavisolanki2000 <pallavi.solanki_cs18@gla.ac.in>
2020-03-28 12:42:42 +0530

    created data

---
+++ src/problem2/main/Methods.java
@@ -0,0 +1,10 @@
+package problem2.main;
+
+import problem1.mybst.MyBinarySearchTree;
+import problem4.myqueue.MyQueue;
+
+public class Methods {
+    private MyBinarySearchTree m;
+
+    private MyQueue pre, post;
+}
