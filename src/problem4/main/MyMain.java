ee3355f1b45f06047d5f70ad4d32f8db7d949918
pallavisolanki2000 <pallavi.solanki_cs18@gla.ac.in>
2020-03-28 13:50:59 +0530

    create scanner class and object

--- src/problem4/main/MyMain.java
+++ src/problem4/main/MyMain.java
@@ -5,7 +5,21 @@
  *  Time: 7:08 PM
  */
 package problem4.main;
+
+import problem1.mybst.MyBinarySearchTree;
+import problem2.main.Methods;
+import problem4.myqueue.MyQueue;
+
+import java.util.Scanner;
+
 // executable class
 // use problem1.mybst.MyBinarySearchTree class for creating binary tree
 public class MyMain {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        Methods mthds = new Methods();
+        MyBinarySearchTree m = new MyBinarySearchTree();
+        MyQueue q = new MyQueue();
+    }
 }
