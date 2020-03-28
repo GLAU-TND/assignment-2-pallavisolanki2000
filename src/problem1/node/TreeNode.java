44260a1782c288ab38c39aa34b44c871b2b34d2a
pallavisolanki2000 <pallavi.solanki_cs18@gla.ac.in>
2020-03-28 12:03:03 +0530

    created constructor

--- src/problem1/node/TreeNode.java
+++ src/problem1/node/TreeNode.java
@@ -10,4 +10,11 @@ public class TreeNode {
     private TreeNode left;
     private TreeNode right;
     private int data;
+
+    public TreeNode(TreeNode left, TreeNode right, int data) {
+        this.left = left;
+        this.right = right;
+        this.data = data;
+
+    }
 }
