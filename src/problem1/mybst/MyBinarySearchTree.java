e3504b0f60b9a9176b7cb93c814611838bde77cb
pallavisolanki2000 <pallavi.solanki_cs18@gla.ac.in>
2020-03-28 12:12:44 +0530

    created  data queue pre and post

--- src/problem1/mybst/MyBinarySearchTree.java
+++ src/problem1/mybst/MyBinarySearchTree.java
@@ -5,6 +5,14 @@
  *  Time: 7:17 PM
  */
 package problem1.mybst;
+
+import problem1.node.TreeNode;
+import problem4.myqueue.MyQueue;
+
 // to implement BinarySearchTree
 public class MyBinarySearchTree {
+    private static int count;
+    private TreeNode newnode, root, tmp;
+    private MyQueue pre;
+    private MyQueue post;
 }
