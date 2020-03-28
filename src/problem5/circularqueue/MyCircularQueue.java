51d7fbcb0d2199d9f46ca5a2a927768afce6b0ba
pallavisolanki2000 <pallavi.solanki_cs18@gla.ac.in>
2020-03-28 14:01:09 +0530

    create print method

--- src/problem5/circularqueue/MyCircularQueue.java
+++ src/problem5/circularqueue/MyCircularQueue.java
@@ -50,4 +50,18 @@ public class MyCircularQueue {
         tmp.setNext(newNode);
 
     }
+
+    public void printQueue() {
+        tmp = front;
+        try {
+            do {
+                System.out.println(tmp.getS().toString());
+                tmp = tmp.getNext();
+            }
+            while (tmp != front && tmp != null);
+        } catch (NullPointerException ignored) {
+        }
+    }
+
+
 }
