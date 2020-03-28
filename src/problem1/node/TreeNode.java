ad4313f8dd0592ea41565d1299629fba9bf90ea5
pallavisolanki2000 <pallavi.solanki_cs18@gla.ac.in>
2020-03-28 12:06:45 +0530

    created  default constructor and getter an setter

--- src/problem1/node/TreeNode.java
+++ src/problem1/node/TreeNode.java
@@ -5,6 +5,7 @@
  *  Time: 7:17 PM
  */
 package problem1.node;
+import java.util.Scanner;
 
 public class TreeNode {
     private TreeNode left;
@@ -16,5 +17,36 @@ public class TreeNode {
         this.right = right;
         this.data = data;
 
+    }
+
+    public TreeNode() {
+        System.out.print("Enter integer value : ");
+        data = new Scanner(System.in).nextInt();
+        left = right = null;
+
+    }
+
+    public TreeNode getLeft() {
+        return left;
+    }
+
+    public void setLeft(TreeNode left) {
+        this.left = left;
+    }
+
+    public TreeNode getRight() {
+        return right;
+    }
+
+    public void setRight(TreeNode right) {
+        this.right = right;
+    }
+
+    public int getData() {
+        return data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
     }
 }
