8b92020c61083008f6746d16be97d3b5443102de
pallavisolanki2000 <pallavi.solanki_cs18@gla.ac.in>
2020-03-28 13:19:02 +0530

    instert method

--- src/problem3/myqueue/MyPriorityQueue.java
+++ src/problem3/myqueue/MyPriorityQueue.java
@@ -12,4 +12,20 @@ public class MyPriorityQueue {
     private Node tmp, front, end;
     private int i = 0;
 
+    public void enqueue(Node newNode) {
+
+        if (front == null || newNode.getS().getRollno() < front.getS().getRollno()) {
+            newNode.setNext(front);
+            front = newNode;
+        } else {
+            tmp = front;
+
+            while (tmp.getNext() != null && tmp.getNext().getS().getRollno() < newNode.getS().getRollno()) {
+                tmp = tmp.getNext();
+            }
+            newNode.setNext(tmp.getNext());
+            tmp.setNext(newNode);
+        }
+    }
+
 }
